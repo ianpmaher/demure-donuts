373dee5f00aff65a2533789c1af4c1147cae1ed8
Ian <ianpmaher@gmail.com>
2024-09-30 00:03:04 -0400

    calendar skeleton

---
+++ app/components/EventsCalendar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Calendar } from "@/components/ui/calendar"
+
+const EventsCalendar = () => {
+  return (
+    <div>
+      <h1>Events Calendar</h1>
+      <Calendar />
+      
+    </div>
+  );
+};
+
+export default EventsCalendar;
--- app/components/MainPage.tsx
+++ app/components/MainPage.tsx
@@ -1,10 +1,18 @@
+import { Calendar } from "@/components/ui/calendar";
+import { Separator } from "@/components/ui/separator";
+
 const MainPage = () => {
   return (
     <div>
       <h1>Main Page</h1>
       <p>This is the main page</p>
+      <div className="w-[50vw]">
+        <Separator />
+      </div>
+      <p>Events Calendar</p>
+      <Calendar />
     </div>
   );
-}
+};
 
 export default MainPage;
