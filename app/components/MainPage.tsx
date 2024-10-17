a64ebcc2801edfd98dfcd02239609e17e165a620
ianpmaher <ianpmaher@gmail.com>
2024-10-16 23:00:17 -0400

    add bento component from magicui

--- app/components/MainPage.tsx
+++ app/components/MainPage.tsx
@@ -4,8 +4,8 @@ import MenuTest from "./MenuTest";
 const MainPage = ({ className }: { className?: string }) => {
   return (
     <div className={`${className} text-center flex flex-col justify-center items-center gap-20`}>
-      <Hero className="flex-1" />
       <MenuTest className="flex-1" />
+      <Hero className="flex-1" />
     </div>
   );
 };
