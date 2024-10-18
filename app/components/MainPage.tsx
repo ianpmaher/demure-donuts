4636120f4c0c02cf9a98e6f3811889e661e2fff4
Ian Maher <ianpmaher@gmail.com>
2024-10-17 21:57:52 -0400

    reformatting main page

--- app/components/MainPage.tsx
+++ app/components/MainPage.tsx
@@ -1,11 +1,33 @@
+import { BentoCard, BentoGrid } from "@/components/ui/bento-grid";
 import Hero from "./Hero";
 import MenuTest from "./MenuTest";
+import Image from "next/image";
+import { DoubleArrowUpIcon, CubeIcon, ChatBubbleIcon, CookieIcon } from "@radix-ui/react-icons";
 
 const MainPage = ({ className }: { className?: string }) => {
+  const images = [
+    { url: "https://i.imgur.com/jYCZgWh.png", description: "Blueberry Swirl", name: "Menu", Icon: CubeIcon, href: "/", cta: "View Menu", classes: "col-span-1" },
+    { url: "https://i.imgur.com/dkOnEcG.png", description: "Choco Latte", name: "Hero", Icon: ChatBubbleIcon, href: "/", cta: "View Hero", classes: "col-span-2" },
+    { url: "https://i.imgur.com/ehqYwDy.png", description: "Birthday Cake", name: "Birthday", Icon: CookieIcon, href: "/", cta: "View Birthday", classes: "col-span-1" },
+    { url: "https://i.imgur.com/4cPsfTF.png", description: "Breakfast Special", name: "Breakfast", Icon: DoubleArrowUpIcon, href: "/", cta: "View Breakfast", classes: "col-span-4" },
+  ];
+
   return (
-    <div className={`${className} text-center flex flex-col justify-center items-center gap-20`}>
-      <MenuTest className="flex-1" />
-      <Hero className="flex-1" />
+    <div className={`${className} text-center p-4`}>
+      <BentoGrid>
+        {images.map((image, index) => (
+          <BentoCard
+            key={index}
+            name={image.name}
+            className={`place-content-center ${image.classes}`}
+            background={<Image src={image.url} alt={image.description} height={500} width={500} />}
+            Icon={image.Icon}
+            description={image.description}
+            href={image.href}
+            cta={image.cta}
+          />
+        ))}
+      </BentoGrid>
     </div>
   );
 };
